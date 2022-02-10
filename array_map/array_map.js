740a6d4d132a5b11ba326487eb3bae7436310865
jepwebbe <jepwebbe@gmail.com>
2022-02-10 14:09:24 +0100

    map_done

---
+++ array_map/array_map.js
@@ -0,0 +1,54 @@
+// Øvelse 1: Lav en fordobling af værdierne i det oprindelige array.
+let arr = [2, 5, 100];
+let arrDouble = arr.map((val, i, arr) => {
+    return val * 2;
+  });
+console.log(arrDouble);
+// Øvelse 2: Tag en række tal og lav dem til strenge
+let arrString = arr.map(() => {
+    return arr.toString();
+  });
+  console.log(arrString)
+// Øvelse 3: Lav navne med stort
+let arrLow = ["hans", "peter", "gringo"]
+let arrUp = arrLow.map(Name => Name.toUpperCase());
+console.log(arrUp);
+// Øvelse 4: Lav et array med navnene
+let arrFamous = [
+    {
+        name: "Mads Mikkelsen",
+        age: 80
+    },
+    {
+        name: "Sofie Gråbøl",
+        age: 2
+    },
+    {
+        name: "Sidse Babett Knudsen",
+        age: 5
+    },
+    {
+        name: "Nikolaj Lie Kaas",
+        age: 16
+    },
+    {
+        name: "Frank Hvam",
+        age: 100
+    }
+]
+
+let arrFamousNames = arrFamous.map(Names => Names.name);
+console.log(arrFamousNames);
+
+//Øvelse 5: Er du gammel nok?
+let arrOldEnough = arrFamous.map(OldEnough => {
+   if (OldEnough.age > 18) {return OldEnough.name + " er gammel nok "};
+   if (OldEnough.age < 18) {return OldEnough.name + " er ikke gammel nok"};
+})
+console.log(arrOldEnough);
+
+//Øvelse 6: Lav et array med navne som h1'ere og alder som h2'ere
+let arrDOM = arrFamous.map(DOMify => {
+    return "<h1>" + DOMify.name + "</h1>" + "<h2>" + DOMify.age + "</h2>";
+})
+console.log(arrDOM);
